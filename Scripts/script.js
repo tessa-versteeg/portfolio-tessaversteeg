61cac89010bd012dc03023f9da3a6f87eb78e43e
tessa-versteeg <70432910+tessa-versteeg@users.noreply.github.com>
2020-11-07 14:57:30 +0100

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+/*eslint-env browser*/
+var hamburgerButton = document.querySelector('.burger');
+var nav = document.querySelector('nav ul');
+
+/*jshint strict: true */
+function toggleMenu() {
+    "use strict";
+    nav.classList.toggle('nav-active');
+}
+
+hamburgerButton.addEventListener('click', toggleMenu);
